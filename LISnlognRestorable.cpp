abea8e60ff4c3660be8155a2ab388a723f56c068
Sameer Raj <42498636+samraj2k@users.noreply.github.com>
2020-12-23 18:39:56 +0530

    LIS-Restorable-nlogn-better-implementation

--- LISnlognRestorable.cpp
+++ LISnlognRestorable.cpp
@@ -1,31 +1,26 @@
-// LIS Restorable
-int solve(vector<int> input, vector<int>& solution)
-{
-    vector<int> magic;
-    vector< vector<int> > history; // OPTIONAL
-    for (int x: input) {
-        vector<int>::iterator it = lower_bound(magic.begin(), magic.end(), x);
-        if (it == magic.end()) {
-            magic.push_back(x);
-            history.push_back(vector<int>(1, x)); // OPTIONAL
-        }
-        else {
-            *it = x;
-            history[it-magic.begin()].push_back(x); // OPTIONAL
+// Type = 0 means Longest Non-decreasing subsequence
+// Type = 1 means Longest Increasing Subsequene
+// a must be 0 based indexing
+// solution vector stores the restored subsequence
+int lis(vector<int> a, vector<int> &solution, int type = 0) {
+    int MAX_INF = 3e18;
+    int n = (int) a.size(), ans = 0;
+    vector<int> b(n + 1, 0), f(n + 1, MAX_INF);
+    for(int i = 0; i < n; i++) {
+        if(!type)
+            f[i + 1] = upper_bound(b.begin() + 1, b.begin() + ans + 1, a[i]) - b.begin();
+        else 
+            f[i + 1] = lower_bound(b.begin() + 1, b.begin() + ans + 1, a[i]) - b.begin();
+        ans = max(ans, f[i + 1]);
+        b[f[i + 1]] = a[i];
+    }
+    int req = ans;
+    for(int i = n; i >= 1; i--) {
+        if(f[i] == req) {
+            solution.push_back(a[i - 1]);
+            req--;
         }
     }
-    
-    // OPTIONAL
-    {
-        int result = magic.size();
-        solution.resize(result);
-        solution.back() = magic.back();
-        for (int i=result-2; i>=0; i--) {
-            auto it = lower_bound(history[i].rbegin(), history[i].rend(), solution[i+1]);
-            it --;
-            solution[i] = *it;
-        }
-    }
-    
-    return magic.size();
+    reverse(solution.begin(), solution.end());
+    return ans;
 }
